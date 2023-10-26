616f6c751a9cde2558ff23006cb2c5a4339dc68b
q.mathieu <q_mathieu@yahoo.fr>
2023-10-26 18:16:35 +0200

    SDK Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.orientationrace"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.orientationrace"
