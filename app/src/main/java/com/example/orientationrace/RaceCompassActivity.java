4a558a45832c40b6d8af47c0aa7c0c0cc441608d
hassen-hichem <hassen.hichem@enis.tn>
2023-11-02 20:38:50 +0100

    bousole_activity

--- app/src/main/java/com/example/orientationrace/RaceCompassActivity.java
+++ app/src/main/java/com/example/orientationrace/RaceCompassActivity.java
@@ -1,57 +1,84 @@
 package com.example.orientationrace;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
-public class RaceCompassActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private CompassView compassView;
+public class RaceCompassActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ImageView compassImage;
     private SensorManager sensorManager;
-    private Sensor orientationSensor;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+
+    private float currentDegree = 0f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_race_compass);
 
-        compassView = findViewById(R.id.compassView);
+        compassImage = findViewById(R.id.compassImageView);
 
+        // Initialize the sensor manager
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        orientationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-
-        if (orientationSensor == null) {
-            // Handle the case where the orientation sensor is not available on the device.
-        }
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(sensorEventListener, orientationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(sensorEventListener);
+        sensorManager.unregisterListener(this);
     }
 
-    private SensorEventListener sensorEventListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            float azimuth = event.values[0];
-            // Update the compass view with the new azimuth value
-            compassView.setAzimuth(azimuth);
-            compassView.invalidate(); // Force a redraw of the view
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == accelerometer) {
+            // Handle accelerometer data
+        } else if (event.sensor == magnetometer) {
+            // Handle magnetometer data
+            float degree = calculateDegree(event.values);
+            rotateCompass(degree);
         }
+    }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
-    };
+    private float calculateDegree(float[] values) {
+        // Calculer l'azimut à partir des données du magnétomètre
+        float azimuth = (float) Math.toDegrees(Math.atan2(values[1], values[0]));
+        return azimuth;
+    }
+
+    private void rotateCompass(float degree) {
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree, degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f
+        );
+        ra.setDuration(250);
+        ra.setFillAfter(true);
+
+        compassImage.startAnimation(ra);
+        currentDegree = degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Gérer les changements d'exactitude des capteurs si nécessaire.
+    }
 }
