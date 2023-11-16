d8d0538ba412af92a5a61532127f4e2512211670
Quentin Mathieu <mathieu@cip.ifi.lmu.de>
2023-11-16 20:35:28 +0100

    Text To Voice

--- app/src/main/java/com/example/orientationrace/activities/RaceCompassActivity.java
+++ app/src/main/java/com/example/orientationrace/activities/RaceCompassActivity.java
@@ -9,10 +9,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import java.util.Locale;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,21 +29,30 @@ import com.example.orientationrace.gardens.Garden;
 import com.example.orientationrace.gardens.GardensAdapter;
 import com.example.orientationrace.gardens.GardensDataset;
 
-public class RaceCompassActivity extends AppCompatActivity implements SensorEventListener {
+public class RaceCompassActivity extends AppCompatActivity implements SensorEventListener, OnInitListener {
 
     // Gardens dataset:
+    private static final String TAG = "TAGListOfGardens, GardenActivity";
     public GardensDataset gardensDataset = new GardensDataset();
 
     private ImageView compassImage;
     private SensorManager sensorManager;
     private Sensor accelerometer;
     private Sensor magnetometer;
-    private Button bCurrentLocation;
-    private boolean isButtonAvailable;
 
     private float currentDegree = 0f;
 
     final GardensAdapter gardensAdapter = new GardensAdapter(gardensDataset, this);
+
+    private TextToSpeech textToSpeech;
+
+    private static final long SPEECH_DELAY_MILLIS = 2000; // Délai de 2 secondes
+    private Handler speechHandler;
+
+    private float correctedDegree ;
+    private Button bCurrentLocation;
+    private boolean isButtonAvailable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,10 +69,17 @@ public class RaceCompassActivity extends AppCompatActivity implements SensorEven
             gardensDataset.addGarden(garden);
         }
 
+        textToSpeech = new TextToSpeech(this, this);
+
         initRecyclerView();
 
         // Applying OnLongClickListener to our Adapter
-        gardensAdapter.setOnLongClickListener((position, garden) -> showPopup(position));
+        gardensAdapter.setOnLongClickListener(new GardensAdapter.OnLongClickListener() {
+            @Override
+            public void onLongClick(int position, Garden garden) {
+                showPopup(position);
+            }
+        });
 
         compassImage = findViewById(R.id.compassImageView);
         bCurrentLocation = findViewById(R.id.buttonCurrentLocation);
@@ -70,15 +90,15 @@ public class RaceCompassActivity extends AppCompatActivity implements SensorEven
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
+        speechHandler = new Handler();
+        startSpeechTimer();
+
         bCurrentLocation.setOnClickListener(v -> {
             if (isButtonAvailable) {
                 startCurrentLocationActivity();
             }
         });
-
     }
-
-
 
     @Override
     protected void onResume() {
@@ -107,17 +127,18 @@ public class RaceCompassActivity extends AppCompatActivity implements SensorEven
 
     private float calculateDegree(float[] values) {
         // Calculer l'azimut à partir des données du magnétomètre
-        values = normalize(values);
+        //values = normalize(values);
         float azimuth = (float) Math.toDegrees(Math.atan2(values[1], values[0]));
         // Correction pour assurer que l'azimut est dans la plage [0, 360)
         azimuth = (azimuth + 360) % 360;
 
         return azimuth;
     }
-    private static final float INITIAL_ROTATION_OFFSET = 270.0f; // Ajustez cette valeur selon votre image
+    private static final float INITIAL_ROTATION_OFFSET = 300.0f; // Ajustez cette valeur selon votre image
 
     private void rotateCompass(float degree) {
-        float correctedDegree = currentDegree + INITIAL_ROTATION_OFFSET;
+        correctedDegree = currentDegree + INITIAL_ROTATION_OFFSET;
+        //speakOrientation(getOrientationLabel(degree));
         RotateAnimation ra = new RotateAnimation(
                 correctedDegree, degree + INITIAL_ROTATION_OFFSET,
                 Animation.RELATIVE_TO_SELF, 0.5f,
@@ -128,6 +149,20 @@ public class RaceCompassActivity extends AppCompatActivity implements SensorEven
 
         compassImage.startAnimation(ra);
         currentDegree = degree;
+    }
+
+    private String getOrientationLabel(float degree) {
+        if (degree >= 0.0f && degree < 80.0f){
+            return "North";
+        }  else if (degree >= 90.0f && degree < 160.0f) {
+            return "East";
+        }  else if (degree >= 180.0f && degree < 260.0f) {
+            return "South";
+        }  else if (degree >= 280.0f && degree < 360.0f) {
+            return "West";
+        } else {
+            return "";
+        }
     }
 
     @Override
@@ -164,15 +199,18 @@ public class RaceCompassActivity extends AppCompatActivity implements SensorEven
 
         // Get reference to the "Confirm" button in the popup layout and add onClick Listener
         Button bConfirm = popupDialog.findViewById(R.id.buttonConfirm);
-        bConfirm.setOnClickListener(v -> {
-            // Update the clicked state for the item
-            gardensAdapter.itemClickedState[position] = true;
+        bConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update the clicked state for the item
+                gardensAdapter.itemClickedState[position] = true;
 
-            // Notify the adapter that the data set has changed
-            gardensAdapter.notifyItemChanged(position);
+                // Notify the adapter that the data set has changed
+                gardensAdapter.notifyItemChanged(position);
 
-            // Dismiss the popup window
-            popupDialog.dismiss();
+                // Dismiss the popup window
+                popupDialog.dismiss();
+            }
         });
 
         // Show the popup
@@ -200,12 +238,44 @@ public class RaceCompassActivity extends AppCompatActivity implements SensorEven
             bCurrentLocation.setText("See current location");
         }, 2 * 10 * 1000); // 5 minutes in milliseconds
     }
-
     private float[] normalize(float[] values) {
         float norm = (float) Math.sqrt(values[0] * values[0] + values[1] * values[1] + values[2] * values[2]);
         values[0] /= norm;
         values[1] /= norm;
         values[2] /= norm;
         return values;
+    }
+
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            // Définir la langue pour le discours (dans cet exemple, anglais)
+            int result = textToSpeech.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+
+            }
+        } else {
+            float speechRate = 0.5f; // Vous pouvez ajuster cette valeur selon vos préférences
+            textToSpeech.setSpeechRate(speechRate);
+        }
+    }
+
+    private void speakOrientation(String orientation) {
+        // Utiliser le TextToSpeech pour lire l'orientation
+        textToSpeech.speak("" + orientation, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+    private void startSpeechTimer() {
+        speechHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Appeler la fonction de synthèse vocale à chaque expiration de la minuterie
+                speakOrientation(getOrientationLabel(correctedDegree-300.0f));
+                // Programmer la prochaine exécution de la minuterie
+                speechHandler.postDelayed(this, SPEECH_DELAY_MILLIS);
+            }
+        }, SPEECH_DELAY_MILLIS);
     }
 }
