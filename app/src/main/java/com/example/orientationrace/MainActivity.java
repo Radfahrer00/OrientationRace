bf6250ba7cc6109ef20f79feb37a7eabea878dc3
Quentin Mathieu <mathieu@cip.ifi.lmu.de>
2023-11-17 12:23:05 +0100

    MainActivity layout

--- app/src/main/java/com/example/orientationrace/MainActivity.java
+++ app/src/main/java/com/example/orientationrace/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.orientationrace;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     // UI Elements
     Button bEnterRace;
     EditText usernameText;
+    TextView usernameLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
         // Get references to UI elements
         bEnterRace = findViewById(R.id.buttonEnterRace);
         usernameText = findViewById(R.id.usernameInput);
+        usernameLabel = findViewById(R.id.usernameLabel);
+
+        usernameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                // Check if the EditText has text, if yes, set the label visible; otherwise, invisible
+                usernameLabel.setVisibility(charSequence.length() > 0 ? View.VISIBLE : View.INVISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
 
         // Listener for the Enter Race Button
         bEnterRace.setOnClickListener(v -> {
